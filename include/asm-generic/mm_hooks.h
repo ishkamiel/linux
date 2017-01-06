723827c36ffa8aa17db17a4a7a7b45f440835809
Mark Rutland <mark.rutland@arm.com>
2017-01-06 16:54:02 +0000

    asm-generic: mm_hooks: allow hooks to be overridden individually
    Currently, an architecture must either implement all of the mm hooks
    itself, or use all of those provided by the asm-generic implementation.
    When an architecture only needs to override a single hook, it must copy
    the stub implementations from the asm-generic version.
    
    To avoid this repetition, allow each hook to be overridden indiviually,
    by placing each under an #ifndef block. As architectures providing their
    own hooks can't include this file today, this shouldn't adversely affect
    any existing hooks.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Cc: linux-arch@vger.kernel.org

--- include/asm-generic/mm_hooks.h
+++ include/asm-generic/mm_hooks.h
@@ -7,30 +7,41 @@
 #ifndef _ASM_GENERIC_MM_HOOKS_H
 #define _ASM_GENERIC_MM_HOOKS_H
 
-static inline void arch_dup_mmap(struct mm_struct *oldmm,
-				 struct mm_struct *mm)
+#ifndef arch_dup_mmap
+static inline int arch_dup_mmap(struct mm_struct *oldmm,
+				struct mm_struct *mm)
 {
 }
+#endif
 
+#ifndef arch_exit_mmap
 static inline void arch_exit_mmap(struct mm_struct *mm)
 {
 }
+#endif
 
+#ifndef arch_unmap
 static inline void arch_unmap(struct mm_struct *mm,
 			struct vm_area_struct *vma,
 			unsigned long start, unsigned long end)
 {
 }
+#endif
 
+#ifndef arch_bprm_mm_init
 static inline void arch_bprm_mm_init(struct mm_struct *mm,
 				     struct vm_area_struct *vma)
 {
 }
+#endif
 
+#ifndef arch_vma_access_permitted
 static inline bool arch_vma_access_permitted(struct vm_area_struct *vma,
 		bool write, bool execute, bool foreign)
 {
 	/* by default, allow everything */
 	return true;
 }
+#endif
+
 #endif	/* _ASM_GENERIC_MM_HOOKS_H */

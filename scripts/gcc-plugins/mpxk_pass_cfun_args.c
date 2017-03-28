b8ffb326151da8d165c255ee3614521c2fda8807
Hans Liljestrand <LiljestrandH@gmail.com>
2017-03-28 15:58:50 +0300

    gcc-plugins: adds MPXK gcc plugin
    Adds a gcc-plugin that modifies vanilla GCC MPX instrumentation for
    in-kernel use. Also adds MPXK_CFLAGS Makefile variable that can be used to
    selectively enable MPXK for specific kernel objects. This plugin is not
    enabled kernel-wide and must be explicitly enabled in the appropriate
    Makefile.
    
    The purpose of the plugin is to replace BNDSTX+BNDLDX bound storage with
    our own mpxk_load_bounds function and to replace memory mainpulating
    function with corresponding wrapper function. The plugin accomplishes
    this via the following tasks:
    
    - It adds MPXK-wrappers functions for memory altering functions, e.g.
      kmalloc, memcpy, etc. This is needed both to check bounds for input
      argument pointers and properly set bounds for returned pointers. This
      is done by the mpxk_wrappers compiler pass. (This could potentially
      be done, for better performance, by direct instrumentation at the call
      site.)
    
    - Replace BNDLDX calls with mpxk_load_bounds function calls. This
      includes two separate cases: free-standing loads handled by the
      mpxk_bnd_store pass, and loads in function prologues that are handled
      by the mpxk_cfun_args pass. These are needed for situation where the
      compile time instrumentation cannot determine a way to statically
      propagate the bounds via the stack or registers.
    
    - The final mpxk_pass_sweeper removes any remaining BNDLDX/BNDSTX calls.
    
    Signed-off-by: Hans Liljestrand <LiljestrandH@gmail.com>
    Signed-off-by: Elena Reshetova <elena.reshetova@intel.com>

---
+++ scripts/gcc-plugins/mpxk_pass_cfun_args.c
@@ -0,0 +1,98 @@
+/*
+ * scripts/gcc-plugins/mpxk_pass_cfun_args.c - MPXK pass for cfun arguments
+ *
+ * Simple pass that only checks the current function (cfun) arguments for
+ * pointer bound counts that exceed the available bndreg count. Such bounds
+ * are then manually initialized at function start using the MPXK bound
+ * load function.
+ *
+ * Copyright (C) 2017 Aalto University
+ *
+ * This file is released under the GPLv2.
+ */
+#include "mpxk.h"
+
+#include <ipa-chkp.h>
+#include <tree-chkp.h>
+
+static unsigned int mpxk_cfun_args_execute(void);
+
+#define PASS_NAME mpxk_cfun_args
+#define NO_GATE
+#include "gcc-generate-gimple-pass.h"
+
+static struct register_pass_info pass_info_mpxk_cfun_args = {
+	.pass				= make_mpxk_cfun_args_pass(),
+	.reference_pass_name		= "optimized",
+	.ref_pass_instance_number	= 1,
+	.pos_op				= PASS_POS_INSERT_BEFORE
+};
+
+struct register_pass_info *get_mpxk_cfun_args_pass_info(void)
+{
+	(void) gcc_version;
+	return &pass_info_mpxk_cfun_args;
+}
+
+static unsigned int mpxk_cfun_args_execute(void)
+{
+	int bound_count = 0;
+	int arg_count = 0;
+	basic_block bb = ENTRY_BLOCK_PTR_FOR_FN(cfun)->next_bb;
+	gimple_stmt_iterator iter = gsi_start_bb(bb);
+	tree list = DECL_ARGUMENTS(cfun->decl);
+	tree prev = NULL;
+
+	if (skip_execute(BND_LEGACY))
+		return 0;
+
+	if (list == NULL || list_length(list) == 0)
+		return 0;
+
+	tree *p;
+
+	for (p = &list; *p; ) {
+		tree l = *p;
+
+		/* Keep count of the encountered bounds args */
+		if (TREE_TYPE(l) == pointer_bounds_type_node) {
+			bound_count++;
+
+			iter = gsi_start_bb(bb);
+
+			/* Replace BNDLDX loaded bounds with mpxk_load_bounds.
+			 *
+			 * This happens in two scenarios:
+			 * - There are more bounds than available registers (>4)
+			 * - The bounds are for arguments beyond the sixth
+			 *   argument, this is *feature* seems to be related to
+			 *   how non-bound arguments are treated (i.e. 6 args
+			 *   are passed via regs before using the stack for
+			 *   arguments).
+			 */
+			if (bound_count > MPXK_BND_REGS || arg_count > 6) {
+				dsay("resetting bound argument #%d (ptr > #6)",
+						bound_count);
+				gcc_assert(prev != NULL);
+				insert_mpxk_bound_load(&iter, prev, l);
+			}
+
+			p = &TREE_CHAIN(l);
+		} else {
+			dsay("skipping non-bound argument #%d", arg_count);
+			prev = l;
+			arg_count++;
+			p = &TREE_CHAIN(l);
+		}
+
+		/* The previous arg is needed if we need a MPXK load. */
+		*p = TREE_CHAIN(l);
+	}
+
+	bound_count = (bound_count <= MPXK_BND_REGS ? 0 :
+			(bound_count - MPXK_BND_REGS));
+
+	mpxk_stats.cfun_ldx += bound_count;
+	dsay("replaced %d bound args with mpkx_load_bounds", bound_count);
+	return 0;
+}

318e7ce54940b104df263cfb2a1d988a9445384e
Hans Liljestrand <liljestrandh@gmail.com>
2017-08-08 11:47:49 +0300

    unaligned: mpxk compatibility (bnd_legacy)

--- include/linux/unaligned/access_ok.h
+++ include/linux/unaligned/access_ok.h
@@ -34,31 +34,37 @@ static __always_inline u64 get_unaligned_be64(const void *p)
 	return be64_to_cpup((__be64 *)p);
 }
 
+__attribute__((bnd_legacy))
 static __always_inline void put_unaligned_le16(u16 val, void *p)
 {
 	*((__le16 *)p) = cpu_to_le16(val);
 }
 
+__attribute__((bnd_legacy))
 static __always_inline void put_unaligned_le32(u32 val, void *p)
 {
 	*((__le32 *)p) = cpu_to_le32(val);
 }
 
+__attribute__((bnd_legacy))
 static __always_inline void put_unaligned_le64(u64 val, void *p)
 {
 	*((__le64 *)p) = cpu_to_le64(val);
 }
 
+__attribute__((bnd_legacy))
 static __always_inline void put_unaligned_be16(u16 val, void *p)
 {
 	*((__be16 *)p) = cpu_to_be16(val);
 }
 
+__attribute__((bnd_legacy))
 static __always_inline void put_unaligned_be32(u32 val, void *p)
 {
 	*((__be32 *)p) = cpu_to_be32(val);
 }
 
+__attribute__((bnd_legacy))
 static __always_inline void put_unaligned_be64(u64 val, void *p)
 {
 	*((__be64 *)p) = cpu_to_be64(val);

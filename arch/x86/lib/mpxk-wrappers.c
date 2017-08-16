8c7a16d1490bccf95c490a54f0fa35d1dfd04888
Hans Liljestrand <liljestrandh@gmail.com>
2017-08-16 12:17:25 +0300

    x86: mpxk-wpappers: track bound hit/miss stats

--- arch/x86/lib/mpxk-wrappers.c
+++ arch/x86/lib/mpxk-wrappers.c
@@ -15,6 +15,45 @@
 
 #include "../../../../scripts/gcc-plugins/mpxk_builtins.def"
 
+struct mpxk_f_stats {
+	long unsigned int miss;
+	long unsigned int hit;
+};
+
+struct mpxk_rt_stats {
+	struct mpxk_f_stats memset;
+	struct mpxk_f_stats memcpy;
+	struct mpxk_f_stats memmove;
+};
+
+struct mpxk_rt_stats mpxk_stats;
+
+#define dump_one(type) pr_info("%s,%lu,%lu\n", #type, mpxk_stats.type.hit, mpxk_stats.type.miss)
+
+void mpxk_dump_stats(void)
+{
+	dump_one(memset);
+	dump_one(memmove);
+	dump_one(memcpy);
+}
+
+inline static bool has_bounds(const void *ptr)
+{
+	if (__bnd_get_ptr_ubound(ptr) != (void *) NULL - 1)
+		return true;
+	if (__bnd_get_ptr_lbound(ptr) != (void *) NULL)
+		return true;
+	return false;
+}
+
+inline static void log_ptr_bounds(struct mpxk_f_stats *f_stat, const void *ptr)
+{
+	if (has_bounds(ptr))
+		f_stat->hit++;
+	else
+		f_stat->miss++;
+}
+
 #undef MPXK_WRAPPER_DEF
 #undef MPXK_BUILTIN_DEF
 
@@ -77,6 +116,9 @@ wrapper3(memmove, void *, void *, d, const void *, s, size_t, c)
 	if (c == 0)
 		return d;
 
+	log_ptr_bounds(&mpxk_stats.memmove, d);
+	log_ptr_bounds(&mpxk_stats.memmove, s);
+
 	__bnd_chk_ptr_bounds(d, c);
 	__bnd_chk_ptr_bounds(s, c);
 
@@ -88,6 +130,9 @@ wrapper3(memcpy, void *, void *, d, const void *, s, size_t,  c)
 {
 	if (c == 0)
 		return d;
+
+	log_ptr_bounds(&mpxk_stats.memcpy, d);
+	log_ptr_bounds(&mpxk_stats.memcpy, s);
 
 	__bnd_chk_ptr_bounds(d, c);
 	__bnd_chk_ptr_bounds(s, c);
@@ -106,6 +151,9 @@ wrapper3(__inline_memcpy, void *, void *, d, const void *, s, size_t,  c)
 	if (c == 0)
 		return d;
 
+	log_ptr_bounds(&mpxk_stats.memcpy, d);
+	log_ptr_bounds(&mpxk_stats.memcpy, s);
+
 	__bnd_chk_ptr_bounds(d, c);
 	__bnd_chk_ptr_bounds(s, c);
 
@@ -118,6 +166,9 @@ wrapper3(__memcpy, void *, void *, d, const void *, s, size_t,  c)
 	if (c == 0)
 		return d;
 
+	log_ptr_bounds(&mpxk_stats.memcpy, d);
+	log_ptr_bounds(&mpxk_stats.memcpy, s);
+
 	__bnd_chk_ptr_bounds(d, c);
 	__bnd_chk_ptr_bounds(s, c);
 
@@ -127,6 +178,8 @@ export_wrapper(__memcpy);
 
 wrapper3(memset, void *, void *, s, int,  c, size_t,  l)
 {
+	log_ptr_bounds(&mpxk_stats.memset, s);
+
 	if (l > 0) {
 		__bnd_chk_ptr_bounds(s, l);
 		wrapper_base(memset)(s, c, l);
